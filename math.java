b129bca05b044c22dade91bf1988f6f6f85057e9
LexLuthor5353 <59502346+LexLuthor5353@users.noreply.github.com>
2025-05-20 19:24:00 +0300

    Add files via upload

---
+++ math.java
@@ -0,0 +1,9 @@
+public class math {
+    public static void main (String[] args) {
+
+        int income = 44000;
+        int spending = 30000;
+        System.out.println (income - spending);
+
+    }
+}
